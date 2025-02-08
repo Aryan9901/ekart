ed3f3d63f0fc125d6eb0195fb6e41d345f9f17c5
Aryan9901 <911aaryan@gmail.com>
2025-02-08 22:28:06 +0530

    created atoms of home module

---
+++ kart/src/modules/home/atoms/Dots.tsx
@@ -0,0 +1,47 @@
+import {View, StyleSheet} from 'react-native';
+import React, {useEffect} from 'react';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from 'react-native-reanimated';
+
+const Dots = ({active, index}: {active: number; index: number}) => {
+  const progress = useSharedValue(0);
+
+  useEffect(() => {
+    if (active === index) {
+      progress.value = withRepeat(withTiming(1, {duration: 3000}), 1, false);
+    } else {
+      progress.value = 0;
+    }
+  }, [active]);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    width: `${progress.value * 100}%`,
+  }));
+
+  return (
+    <View style={[styles.dotContainer, {width: active === index ? 35 : 20}]}>
+      <Animated.View style={[styles.activeDot, animatedStyle]} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  dotContainer: {
+    height: 4,
+    borderRadius: 50,
+    backgroundColor: '#DFDFDF',
+    overflow: 'hidden',
+    marginHorizontal: 5,
+  },
+  activeDot: {
+    height: '100%',
+    backgroundColor: '#000',
+    borderRadius: 50,
+  },
+});
+
+export default Dots;
