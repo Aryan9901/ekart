7c86940e4f27db6196c51a5b3947e3730ba0327e
Aryan9901 <911aaryan@gmail.com>
2025-02-08 22:27:16 +0530

    setting home module index file

--- kart/src/App.tsx
+++ kart/src/App.tsx
@@ -1,8 +1,14 @@
 import Navigation from '@navigation/Navigation';
 import React from 'react';
+import {Provider} from 'react-redux';
+import {store} from '@store/store';
 
 function App(): React.JSX.Element {
-  return <Navigation />;
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  );
 }
 
 export default App;
