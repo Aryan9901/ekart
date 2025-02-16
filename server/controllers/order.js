37e8f587a3bbc741cbaa7eb5082957133dd57f44
ARYAN GUPTA <911aaryan@gmail.com>
2025-02-16 18:39:38 +0530

    Update order.js

--- server/controllers/order.js
+++ server/controllers/order.js
@@ -5,7 +5,6 @@ import Razorpay from "razorpay";
 
 export const createTransaction = async (req, res) => {
   const { amount, userId } = req.body;
-  
   const razorpay = new Razorpay({
     key_id: process.env.RAZOR_PAY_KEY_ID,
     key_secret: process.env.RAZOR_PAY_SECRET,
