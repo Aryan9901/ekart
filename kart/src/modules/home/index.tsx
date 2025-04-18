c96d3fa187285eacb3ff212beb405c8786c71c7a
ARYAN GUPTA <911aaryan@gmail.com>
2025-04-18 21:02:58 +0530

    Update index.tsx

--- kart/src/modules/home/index.tsx
+++ kart/src/modules/home/index.tsx
@@ -14,6 +14,7 @@ import MenuHeader from './molecules/MenuHeader';
 import SearchBar from './molecules/SearchBar';
 import MainList from './templates/MainList';
 
+
 const Home = () => {
   const insets = useSafeAreaInsets();
   const scrollYGlobal = useSharedValue(0);
