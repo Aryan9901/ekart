2aa9bdb281a02174b5e572a120a5131453eb148f
ARYAN GUPTA <911aaryan@gmail.com>
2025-04-20 15:28:14 +0530

    Update getProducts.tsx

--- kart/src/modules/products/api/getProducts.tsx
+++ kart/src/modules/products/api/getProducts.tsx
@@ -1,7 +1,6 @@
 import {BASE_URL} from '@store/config';
 import axios from 'axios';
 
-
 export const getProductsByCategory = async (id: any) => {
   try {
     const res = await axios.get(`${BASE_URL}/product/${id}`);
