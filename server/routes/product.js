d3eff64cf4c428b80e9dc890dabc429dacb761b8
ARYAN GUPTA <911aaryan@gmail.com>
2025-05-20 14:44:54 +0530

    Update product.js

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { getProductByCategoryId } from "../controllers/product.js";
-
 const router = Router();
+
 
 router.route("/:category").get(getProductByCategoryId);
 
