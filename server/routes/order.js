8d4cbd2cb4a0441801fce939bfe4cac901f40a5a
ARYAN GUPTA <911aaryan@gmail.com>
2025-05-20 14:44:32 +0530

    Update order.js

--- server/routes/order.js
+++ server/routes/order.js
@@ -4,8 +4,8 @@ import {
   createTransaction,
   getOrdersByUserId,
 } from "../controllers/order.js";
-
 const router = Router();
+
 router.route("/").post(createOrder);
 router.route("/:userId").get(getOrdersByUserId);
 router.route("/transaction").post(createTransaction);
