a2210db6d1c921d9d9f39b670ecea2a3a9b6f7eb
ARYAN GUPTA <911aaryan@gmail.com>
2025-07-12 11:41:52 +0530

    Update reduxHook.tsx

--- kart/src/store/reduxHook.tsx
+++ kart/src/store/reduxHook.tsx
@@ -3,5 +3,7 @@ import {RootState, AppDispatch} from './store';
 
 
 
+
+
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
